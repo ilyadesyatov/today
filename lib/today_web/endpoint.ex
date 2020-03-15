6f4a56dc6ff03ee856fc28b9c11e41a0229258f7
ilyadesyatov <chirikwork@gmail.com>
2020-03-15 14:59:08 +0300

    Preparing to upload on heroku

--- lib/today_web/endpoint.ex
+++ lib/today_web/endpoint.ex
@@ -2,7 +2,7 @@ defmodule TodayWeb.Endpoint do
   use Phoenix.Endpoint, otp_app: :today
 
   socket "/socket", TodayWeb.UserSocket,
-    websocket: true,
+    websocket: [timeout: 45_000],
     longpoll: false
 
   # Serve at "/" the static files from "priv/static" directory.
